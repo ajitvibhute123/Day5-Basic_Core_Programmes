3a8a214b1ae8b2e6965955f5ec64f32eddb0191e
ajitvibhute123 <vibhuteajit3@gmail.com>
2022-03-29 23:55:52 +0530

    Basic Core Programme

---
+++ WindChill_Problem.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class WindChill_Problem {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+        System.out.println("Enter the temperature in Fahrenheit " + "between -58 F: ");
+        double temperature = input.nextDouble();
+        System.out.println("Enter the wind speed (>= 2) in miles per hour: ");
+        double speed = input.nextDouble();
+
+        double windchill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temperature * Math.pow(speed, 0.16);
+
+        //Display result
+        System.out.println("The wind chill index is " +windchill);
+
+
+	}
+
+}
